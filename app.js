8f8837fa1a1cb12b828f6709659d3392db8d889a
Timryan3001 <timryan3001@gmail.com>
2024-06-13 15:02:28 +0100

    Adding in functionality with buttons

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ let vizActiveSheet;
 let dashboard;
 let listSheets;
 
+// creating variables for worksheets
+let saleMap;
+let totalSales;
+let salesByProduct;
+let salesBySegment;
+
+
 //Create a function which will log information about the workbook
 
 function logWorkbookInformation() {
@@ -30,6 +37,54 @@ function logWorkbookInformation() {
         index = element.index;
         console.log(`The worksheet with index [${index}] is "${element.name}"`)
     })
+
+    //assign sheets to variables
+    saleMap = listSheets.find((ws) => ws.name == "SaleMap");
+    totalSales = listSheets.find((ws) => ws.name == "Total Sales");
+    salesByProduct = listSheets.find((ws) => ws.name == "SalesbyProduct");
+    salesBySegment = listSheets.find((ws) => ws.name == "SalesbySegment");
 }
 
 viz.addEventListener("firstinteractive", logWorkbookInformation);
+
+
+// Making the buttons work
+// Tell JS Which buttons to look for
+
+const oregonWashingtonButton = document.getElementById("oregon_and_washington")
+const clearFilterButton = document.getElementById("clear_filter")
+const undoButton = document.getElementById("undo")
+
+function oregonWashFunction() {
+    console.log(oregonWashingtonButton.value)
+
+    //apply filters to sheets
+    saleMap.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    totalSales.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesByProduct.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+    salesBySegment.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+}
+
+function clearStateFilter() {
+    //log what's pressed 
+    console.log(clearFilterButton.value);
+
+    saleMap.clearFilterAsync("State");
+    totalSales.clearFilterAsync("State");
+    salesByProduct.clearFilterAsync("State");
+    salesBySegment.clearFilterAsync("State");
+}
+
+function undo() {
+    //log whats being pressed
+    console.log(undoButton.value);
+    viz.undoAsync();
+}
+
+//event listener for my button
+oregonWashingtonButton.addEventListener("click", oregonWashFunction);
+clearFilterButton.addEventListener("click", clearStateFilter);
+undoButton.addEventListener("click", undo);
+
+
+
